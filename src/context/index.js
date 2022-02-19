d1ad8f5b95946dc6b131f560c9f3270a803437ab
Ife Fatolu <joefatolu>
2022-02-19 15:27:03 +0100

    made a chnage

--- src/context/index.js
+++ src/context/index.js
@@ -1,16 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./auth-context";
-import { GlobalStyle } from "../styles/GlobalStyles";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 function AppProviders({ children }) {
   return (
     <Router>
-      <GlobalStyle />
       <AuthProvider>{children}</AuthProvider>
-      <ToastContainer />
     </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { AppProviders } from "./context";
+import { GlobalStyle } from "./styles/GlobalStyles";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <AppProviders>
       <App />
+      <GlobalStyle />
+      <ToastContainer />
     </AppProviders>
   </React.StrictMode>,
   document.getElementById("root")
